53fb5d1ffcfbcbc8946042ca0116e7fe563580eb
Mariana Schmitt <marianaschmit09@gmail.com>
2025-05-06 11:19:53 -0400

    Implementação completa do sistema de conversão.

---
+++ mavenproject1/src/main/java/poo/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.mavenproject1;
+
+/**
+ *
+ * @author 2024105070059
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
