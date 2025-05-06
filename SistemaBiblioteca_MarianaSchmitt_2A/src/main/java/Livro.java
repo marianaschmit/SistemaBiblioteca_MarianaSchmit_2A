8a8720ab06b0f43fe92f1a1c737fc0c3bf66cd48
mariana <marianaschmit09@gmail.com>
2025-05-06 11:08:50 -0400

    Update Livro.java

--- SistemaBiblioteca_MarianaSchmitt_2A/src/main/java/Livro.java
+++ SistemaBiblioteca_MarianaSchmitt_2A/src/main/java/Livro.java
@@ -1,3 +1,21 @@
 public class Livro {
-    
+    //atributos
+    private static int proximoIsbn = 1;
+    private int isbn;
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    //construtor
+    public Livro(String titulo, String autor, int ano){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.isbn =proximoIsbn++;
+    }
+
+    //método getter
+    public String getTitulo(){
+        return this.titulo;
+    }
 }
